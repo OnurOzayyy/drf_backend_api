3ee29f56a76a23d8e8188a189efdc8db6449b9ac
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-06-25 21:37:12 -0700

    display products in category

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
 
 class CategorySerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='category_detail-api')
+    product_set = ProductSerializer(many=True)
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = [
+            "id",
+            "url",
+            "title",
+            "description",
+            "slug",
+            "active",
+            "product_set"]
