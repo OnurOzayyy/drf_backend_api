8f8602947b567db31adec040c515fb0d048c6bfa
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-07-05 20:18:11 -0700

    Add Default Permission to Allow Any

--- src/shop/settings.py
+++ src/shop/settings.py
@@ -132,3 +132,9 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
