84b4d4fa94d0eab1d1afb1f084985e1740c149c9
Onur Ozay <onurozay@onurs-mbp.attlocal.net>
2018-07-06 17:48:11 -0700

    Basic Authentication

--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from .serializers import CategorySerializer, ProductSerializer, ProductDetailSerializer
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+from .serializers import CategorySerializer, ProductSerializer, ProductDetailSerializer
 from .models import Category, Product
 
 
@@ -22,6 +24,8 @@ class CategoryListAPIView(generics.ListCreateAPIView):
 
 
 class CategoryRetrieveAPIView(generics.RetrieveAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
